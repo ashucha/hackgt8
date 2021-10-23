974870109f2e732abd1fd2d88590d36fa792d3cb
Ashu <ashucha03@gmail.com>
2021-10-23 13:58:31 -0400

    Starter

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("Hello");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const axios = require("axios");
